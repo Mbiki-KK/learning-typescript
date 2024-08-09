4491f6a3a926a1fee312e06522d4c8357345239e
Mbiki-KK <wawerumbiki@gmail.com>
2024-08-09 20:51:53 +0300

    middle of lesson 4

--- src/main.ts
+++ src/main.ts
@@ -1,67 +1,16 @@
-let shotguns = ["Pump", "Auto shotgun", "Combat", "Tactical"]
+// Type Aliases
+type booleanOrString = (boolean | string)
 
-let digit = [2, 3, 7, 11, 21, 5492, 829, 102]
-
-let address = [204, "Marley Drive"] // Union types
-
-let signal = [1, "On", true] // also a union type
-
-
-//Tuples -arrays with definite length and definite data types
-let myTuple: [string, number, boolean]
-
-myTuple = ["hey", 56, false]
-
-let mixed = ["hey", 56, false]
-// mixed = myTuple - works
-// myTuple = mixed - does not work
-
-// Objects
-
-let player: object
-
-let gamer = {}
-
-player = gamer // --true
-
-// Type vs Interface
-type Guitarist = {
-    name: string,
-    active: boolean,
-    albums: (string | number)[]
-}
-
-interface Lawyer {
+type Lawyer = {
     name?: string,
-    active: (boolean | string),
+    active: booleanOrString ,
     casesDone: number
 }
 
-// In type, you should use an equals sign(=) while in interface, you do not use it.
+// Literal Types
+let username: "Granslik"
+
+// username = Miquella -- doesn't work
 
 
-// enum - a methos that enumerates values in it
-enum Grade {
-    U = 1,
-    D,
-    C,
-    B,
-    A
-}
-console.log(Grade.A)
 
-
-const greetLawyer = (lawyer: Lawyer) => {
-    if (lawyer.name) {
-        return `Hello ${lawyer.name.toUpperCase()}!`
-    }
-    return "Hello!"
-}
-
-let KK: Lawyer = {
-    name: "Mbiki Kamanjiri",
-    active: true,
-    casesDone: 70
-}
-
-console.log(greetLawyer(KK))
