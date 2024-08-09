71ab98a8e43dab8862c147b5c510a505c0730431
Mbiki-KK <wawerumbiki@gmail.com>
2024-08-09 17:00:39 +0300

    lesson 3

--- build/js/main.js
+++ build/js/main.js
@@ -1,9 +1,38 @@
 "use strict";
-let myName = "Mbiki";
-let address;
-address = 184;
-address = "204 Mayple street";
-const sum = (a, b) => {
-    return a + b;
+let shotguns = ["Pump", "Auto shotgun", "Combat", "Tactical"];
+let digit = [2, 3, 7, 11, 21, 5492, 829, 102];
+let address = [204, "Marley Drive"]; // Union types
+let signal = [1, "On", true]; // also a union type
+//Tuples -arrays with definite length and definite data types
+let myTuple;
+myTuple = ["hey", 56, false];
+let mixed = ["hey", 56, false];
+// mixed = myTuple - works
+// myTuple = mixed - does not work
+// Objects
+let player;
+let gamer = {};
+player = gamer; // --true
+// In type, you should use an equals sign(=) while in interface, you do not use it.
+// enum - a methos that enumerates values in it
+var Grade;
+(function (Grade) {
+    Grade[Grade["U"] = 1] = "U";
+    Grade[Grade["D"] = 2] = "D";
+    Grade[Grade["C"] = 3] = "C";
+    Grade[Grade["B"] = 4] = "B";
+    Grade[Grade["A"] = 5] = "A";
+})(Grade || (Grade = {}));
+console.log(Grade.A);
+const greetLawyer = (lawyer) => {
+    if (lawyer.name) {
+        return `Hello ${lawyer.name.toUpperCase()}!`;
+    }
+    return "Hello!";
 };
-console.log(sum(4, 3));
+let KK = {
+    name: "Mbiki Kamanjiri",
+    active: true,
+    casesDone: 70
+};
+console.log(greetLawyer(KK));
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,67 @@
-let myName = "Mbiki"
-let address: string | number
-address = 184
-address = "204 Mayple street"
+let shotguns = ["Pump", "Auto shotgun", "Combat", "Tactical"]
 
-const sum = (a:number ,b:number) => {
-    return a+b
- }
-console.log(sum(4,3))
+let digit = [2, 3, 7, 11, 21, 5492, 829, 102]
+
+let address = [204, "Marley Drive"] // Union types
+
+let signal = [1, "On", true] // also a union type
 
 
+//Tuples -arrays with definite length and definite data types
+let myTuple: [string, number, boolean]
+
+myTuple = ["hey", 56, false]
+
+let mixed = ["hey", 56, false]
+// mixed = myTuple - works
+// myTuple = mixed - does not work
+
+// Objects
+
+let player: object
+
+let gamer = {}
+
+player = gamer // --true
+
+// Type vs Interface
+type Guitarist = {
+    name: string,
+    active: boolean,
+    albums: (string | number)[]
+}
+
+interface Lawyer {
+    name?: string,
+    active: (boolean | string),
+    casesDone: number
+}
+
+// In type, you should use an equals sign(=) while in interface, you do not use it.
+
+
+// enum - a methos that enumerates values in it
+enum Grade {
+    U = 1,
+    D,
+    C,
+    B,
+    A
+}
+console.log(Grade.A)
+
+
+const greetLawyer = (lawyer: Lawyer) => {
+    if (lawyer.name) {
+        return `Hello ${lawyer.name.toUpperCase()}!`
+    }
+    return "Hello!"
+}
+
+let KK: Lawyer = {
+    name: "Mbiki Kamanjiri",
+    active: true,
+    casesDone: 70
+}
+
+console.log(greetLawyer(KK))
