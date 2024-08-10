bfad1ffa69e0d3d730188e41c3942d20741be3d7
Mbiki-KK <wawerumbiki@gmail.com>
2024-08-10 16:26:46 +0300

    end of lesson 4

--- build/js/main.js
+++ build/js/main.js
@@ -1,38 +1,47 @@
 "use strict";
-let shotguns = ["Pump", "Auto shotgun", "Combat", "Tactical"];
-let digit = [2, 3, 7, 11, 21, 5492, 829, 102];
-let address = [204, "Marley Drive"]; // Union types
-let signal = [1, "On", true]; // also a union type
-//Tuples -arrays with definite length and definite data types
-let myTuple;
-myTuple = ["hey", 56, false];
-let mixed = ["hey", 56, false];
-// mixed = myTuple - works
-// myTuple = mixed - does not work
-// Objects
-let player;
-let gamer = {};
-player = gamer; // --true
-// In type, you should use an equals sign(=) while in interface, you do not use it.
-// enum - a methos that enumerates values in it
-var Grade;
-(function (Grade) {
-    Grade[Grade["U"] = 1] = "U";
-    Grade[Grade["D"] = 2] = "D";
-    Grade[Grade["C"] = 3] = "C";
-    Grade[Grade["B"] = 4] = "B";
-    Grade[Grade["A"] = 5] = "A";
-})(Grade || (Grade = {}));
-console.log(Grade.A);
-const greetLawyer = (lawyer) => {
-    if (lawyer.name) {
-        return `Hello ${lawyer.name.toUpperCase()}!`;
+// Literal Types
+let username;
+// username = Miquella -- doesn't work
+// functions
+const add = (a, b) => {
+    return a + b;
+};
+const logMessage = (messange) => {
+    console.log(messange);
+};
+logMessage("Hello there");
+logMessage(add(2, 3));
+logMessage("Hello there");
+// interface mathFunction {(a: number, b:number): number}
+// For sake of habit, use type for type literals and interface for class definition
+let subtract = function (c, d) {
+    return c - d;
+};
+logMessage(subtract(2, 3));
+let multiply = function (f, g) {
+    return f * g;
+};
+logMessage(multiply(4, 6));
+// Optional parameters
+const addAll = (a, b, c) => {
+    // typeguard - avoids error of possibly undefined
+    if (typeof c !== 'undefined') {
+        return a + b + c;
     }
-    return "Hello!";
+    return a + b;
 };
-let KK = {
-    name: "Mbiki Kamanjiri",
-    active: true,
-    casesDone: 70
+// default param value
+const sumAll = (a, b, c = 2) => {
+    return a + b + c;
 };
-console.log(greetLawyer(KK));
+logMessage(addAll(2, 3, 4));
+logMessage(addAll(2, 3));
+logMessage(sumAll(2, 3));
+// Rest parameters
+const total = (...nums) => {
+    return nums.reduce((prev, curr) => prev + curr);
+};
+logMessage(total(1, 2, 3, 4));
+const createError = (errMsg) => {
+    throw new Error(errMsg);
+};
--- src/main.ts
+++ src/main.ts
@@ -14,3 +14,65 @@ let username: "Granslik"
 
 
 
+// functions
+const add = (a: number,b: number): number => {
+    return a+b
+}
+
+const logMessage = (messange:any): void => {
+    console.log(messange)
+}
+logMessage("Hello there")
+logMessage(add(2,3))
+logMessage("Hello there")
+
+type mathFunction = (a:number, b:number) => number
+
+// interface mathFunction {(a: number, b:number): number}
+
+// For sake of habit, use type for type literals and interface for class definition
+
+
+let subtract: mathFunction = function (c,d) {
+    return c-d
+}
+logMessage(subtract(2,3))
+
+let multiply: mathFunction = function (f,g) {
+    return f * g
+}
+
+logMessage(multiply(4,6))
+
+// Optional parameters
+
+const addAll = (a: number, b:number, c?:number): number => {
+    // typeguard - avoids error of possibly undefined
+    if (typeof c !== 'undefined') {
+        return a+b+c
+    }
+    return a+b
+}
+// default param value
+const sumAll = (a: number, b:number, c:number = 2): number => {
+
+    return a+b+c
+}
+
+
+logMessage(addAll(2,3,4))
+logMessage(addAll(2,3))
+logMessage(sumAll(2,3))
+
+// Rest parameters
+
+const total = (...nums: number[]):number => {
+    return nums.reduce((prev, curr) => prev + curr)
+}
+
+logMessage(total(1,2,3,4))
+
+const createError = (errMsg: string) => {
+    throw new Error(errMsg)
+}
+
